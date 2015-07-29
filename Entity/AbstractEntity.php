404c18883d7a426862f60805830c6eb146ed12f4
Wolfram Eberius <edrush@posteo.de>
2015-07-29 15:30:25 +0200

    Added AbstractEntity and documentation

---
+++ Entity/AbstractEntity.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Xima\CoreBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\ORM\Mapping\MappedSuperclass;
+use Knp\DoctrineBehaviors\Model as ORMBehaviors;
+
+/**
+ * Entity
+ *
+ * @MappedSuperclass
+ */
+abstract class AbstractEntity
+{
+    use ORMBehaviors\Timestampable\Timestampable;
+    use ORMBehaviors\SoftDeletable\SoftDeletable;
+    
+    /**
+     * @var boolean
+     *
+     * @ORM\Column(name="is_active", type="boolean", nullable=true)
+     */
+    protected $isActive = true;
+
+    
+    /**
+     * Set id
+     *
+     * @param int $id
+     * @return Entity
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     * @return Entity
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+
+        return $this;
+    }
+
+    /**
+     * Get isActive
+     *
+     * @return boolean 
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-Core bundle
-===========
+# Core Bundle
+
+## Features
+*  **AbstractEntity**: base properties and behaviours (*doctrine/orm*, *knplabs/doctrine-behaviors*)
+* Support for **CronTasks**: see [Creating automated, interval-based "cron" tasks in Symfony2](https://inuits.eu/blog/creating-automated-interval-based-cron-tasks-symfony2)  (*symfony/framework-bundle*, *symfony/console*, *symfony/doctrine-bridge*)
+* **BeforeControllerListener**: InitializableControllerInterface: Controller->initialize(Request $request, SecurityContextInterface $security_context) (*symfony/http-foundation*, *symfony/security*)
+* Service **OGDBWrapper** and model class for **GeoCode** (*symfony/framework-bundle*, *symfony/dependency-injection*)
+* FormTypes **DatePickerType** and **TimePickerType** (*symfony/form*, *symfony/options-resolver*, *sonata-project/core-bundle*)
