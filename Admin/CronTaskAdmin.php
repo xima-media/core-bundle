ace0f8fefaecb64a6c5b618b7e601b6076e1543b
Wolfram Eberius <woe@xima.de>
2015-09-08 17:23:33 +0200

    Added SonataAdmin for Crontask entity

---
+++ Admin/CronTaskAdmin.php
@@ -0,0 +1,35 @@
+<?php
+namespace Xima\CoreBundle\Admin;
+
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Route\RouteCollection ;
+
+class CronTaskAdmin extends Admin
+{
+    // Fields to be shown on filter forms
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper
+            ->add('name')
+        ;
+    }
+
+    // Fields to be shown on lists
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper
+            ->add('name')
+            ->add('commands')
+            ->add('interval')
+            ->add('lastrun')
+        ;
+    }
+
+    protected function configureRoutes(RouteCollection $collection)
+    {
+        $collection->clearExcept(array('list'));
+    }
+}
