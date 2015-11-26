be81df42328ed13f4f402117980c5c9792b62d8c
Wolfram Eberius <woe@xima.de>
2015-11-26 11:06:10 +0100

    Allow extension of ExportEntitiesCommand

--- Command/ExportEntitiesCommand.php
+++ Command/ExportEntitiesCommand.php
@@ -1,6 +1,9 @@
 <?php
 namespace Xima\CoreBundle\Command;
 
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\QueryBuilder;
+use Doctrine\ORM\Tools\Pagination\Paginator;
 use JMS\Serializer\Naming\IdenticalPropertyNamingStrategy;
 use JMS\Serializer\SerializerBuilder;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
@@ -12,6 +15,21 @@ use Symfony\Component\Filesystem\Filesystem;
 
 class ExportEntitiesCommand extends ContainerAwareCommand
 {
+    /**
+     * @var integer
+     */
+    protected $limit;
+
+    /**
+     * @var integer
+     */
+    protected $offset;
+
+    /**
+     * @var string
+     */
+    protected $entity;
+
     const EXPORT_FOLDER = 'export';
 
     protected function configure()
@@ -20,31 +38,30 @@ class ExportEntitiesCommand extends ContainerAwareCommand
             ->setName('xima:export-entities')
             ->setDescription('A command to export all entities of a specified type to a json file. Can be done incrementally.')
             ->addArgument('entity', InputArgument::REQUIRED, 'The database you want to export')
-            ->addOption('limit', 'l', InputOption::VALUE_OPTIONAL, 'The database query limit user', '500')
-        ;
+            ->addOption('limit', 'l', InputOption::VALUE_OPTIONAL, 'The database query limit user', '500');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $entity = $input->getArgument('entity');
-        $limit = $input->getOption('limit');
+        $this->entity = $input->getArgument('entity');
+        $this->limit = $input->getOption('limit');
 
-        if (!class_exists ($entity)) {
-            $output->writeln('<error>Error!</error> Class \''.$entity.'\' not found.');
+        if (!class_exists($this->entity)) {
+            $output->writeln('<error>Error!</error> Class \'' . $this->entity . '\' not found.');
             return;
         }
 
         $em = $this->getContainer()->get('doctrine')->getManager();
-        $repository = $em->getRepository($entity);
+        $repository = $em->getRepository($this->entity);
         /* @var $repository \Doctrine\ORM\EntityRepository */
 
-        if (!is_a ($repository, '\Doctrine\ORM\EntityRepository')) {
+        if (!is_a($repository, '\Doctrine\ORM\EntityRepository')) {
             $output->writeln('<error>Error!</error> No EntityRepository found.');
             return;
         }
         // configuration and setup
         $fs = new Filesystem();
-        $reflection = $em->getClassMetadata($entity)->getReflectionClass();
+        $reflection = $em->getClassMetadata($this->entity)->getReflectionClass();
         /* @var $reflection \ReflectionClass */
 
         $serializer = SerializerBuilder::create()
@@ -52,14 +69,14 @@ class ExportEntitiesCommand extends ContainerAwareCommand
             ->build();
 
         // export directory
-        $dir = $this->getContainer()->get('kernel')->getRootDir().'/'.self::EXPORT_FOLDER.'/'.str_replace('\\', '/', $reflection->getName());
+        $dir = $this->getContainer()->get('kernel')->getRootDir() . '/' . self::EXPORT_FOLDER . '/' . str_replace('\\', '/', $reflection->getName());
         if (!$fs->exists($dir)) {
             $fs->mkdir($dir);
         }
 
-        $fileName = $reflection->getShortName().'.json';
-        $file = $dir.'/'.$fileName;
-        $tmpFile = $dir.'/_'.$fileName;
+        $fileName = $reflection->getShortName() . '.json';
+        $file = $dir . '/' . $fileName;
+        $tmpFile = $dir . '/_' . $fileName;
 
         // do the export only if it was not completed today
         if ($fs->exists($file) && date('F d Y', strtotime('today')) == date('F d Y', filemtime($file))) {
@@ -67,12 +84,14 @@ class ExportEntitiesCommand extends ContainerAwareCommand
             return;
         }
 
-        $offsetFile = $dir.'/last_offset';
+        $offsetFile = $dir . '/last_offset';
         // the offset is 0 or the last one used + limit
-        $offset = ($fs->exists($offsetFile)) ? file_get_contents($offsetFile) : 0;
+        $this->offset = ($fs->exists($offsetFile)) ? file_get_contents($offsetFile) : 0;
 
-        // now get the entities from db
-        $result = $repository->findBy(array(), array(), $limit, $offset);
+        $qb = $this->createQueryBuilder($em);
+        $paginator = new Paginator($qb);
+
+        $result = $paginator->getQuery()->getResult();
         $data = json_decode($serializer->serialize($result, 'json'), true);
 
         if ($fs->exists($tmpFile)) {
@@ -81,20 +100,38 @@ class ExportEntitiesCommand extends ContainerAwareCommand
         $jsonData = json_encode($data);
 
         $count = count($result);
-        $countTotal=$count+$offset;
+        $countTotal = $count + $this->offset;
 
-        if ($count == $limit) {
+        if ($count == $this->limit) {
             //still more in the db
             $fs->dumpFile($tmpFile, $jsonData);
             $fs->dumpFile($offsetFile, $countTotal);
-            $output->writeln('<comment>Good</comment> - exported '.$count. ' entities of type \''.$reflection->getName().'\' - now having '.$countTotal.', waiting for more.');
+            $output->writeln('<comment>Good</comment> - exported ' . $count . ' entities of type \'' . $reflection->getName() . '\' - now having ' . $countTotal . ' of '.count($paginator).', waiting for more.');
         } else {
             //export completed
             $fs->dumpFile($file, $jsonData);
             $fs->remove($offsetFile);
             $fs->remove($tmpFile);
 
-            $output->writeln('<info>Done</info> - exported all '.$count.' entities of type \''.$reflection->getName().'\'.');
+            $output->writeln('<info>Done</info> - exported all ' . $countTotal . ' entities of type \'' . $reflection->getName() . '\'.');
         }
+    }
+
+    /**
+     * @param EntityManagerInterface $em
+     * @return QueryBuilder
+     */
+    protected function createQueryBuilder(EntityManagerInterface $em)
+    {
+        $qb = $em->createQueryBuilder();
+
+        /* @var $qb \Doctrine\ORM\QueryBuilder */
+        $qb
+            ->select('x')
+            ->from($this->entity, 'x')
+            ->setFirstResult($this->offset)
+            ->setMaxResults($this->limit);
+
+        return $qb;
     }
 }
